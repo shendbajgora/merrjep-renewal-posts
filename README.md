2b9efd1d948de112a303536f9472cac264c17683
Shpend Bajgora <shpendbajgora91@gmail.com>
2020-01-05 15:07:52 +0100

    Merrjep auto renewal posts

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # merrjep-renewal-posts
 
+Merrjep auto renewal posts (works only for merrjep).
+
+### Requirements: ###
+
+* Browser: Chrome
+* Chrome extension 'cjs'
+* Take code from merrjep.js, and place it inside 'cjs'
+
 ## Installation ## 
 
 1. Install google chrome extension 'cjs': https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija?hl=en
--- merrjep.js
+++ merrjep.js
@@ -2,27 +2,26 @@
 // Step 2: Insert code inside cjs
 // Step 3: Have fun (works only for merrjep renewal posts)
 
-var wind = window;
-
-var domain = wind.location.hostname;
+var wind = window,
+	domain = wind.location.hostname;
 
 if (domain.indexOf('merrjep') > -1) {	  
+	
 	initScript();
 }
 
 // Init script
 function initScript() {
 
-	var path = wind.location.pathname;
 	var url = decodeURIComponent(window.location.href);
 
 	// Confirm page
-	if (path.toLowerCase().indexOf('konfirmimi') > -1) {
+	if (url.toLowerCase().indexOf('konfirmimi') > -1) {
 		closePage();
 	}
 
 	// Renewal page
-	else if (path.toLowerCase().indexOf('rregullim-i-shpalljes') > -1) {
+	else if (url.toLowerCase().indexOf('rregullim-i-shpalljes') > -1) {
 		renewalPost();
 	}
 
@@ -35,19 +34,21 @@ function initScript() {
 
 // Close page
 function closePage() {
+	
 	wind.close();
 }
-
 
 // Renewal post
 function renewalPost() {
 
 	document.querySelector('input[value="Renewal"]').click();
 
-	setTimeout(function(){
+	setTimeout(function() {
+		
 		var btns = document.querySelectorAll('input[type="submit"]');
 
 		btns.forEach(function(btn) {
+			
 			btn.click();
 		});
 	}, 100);
@@ -55,6 +56,7 @@ function renewalPost() {
 
 // Loop posts
 function loopPosts() {
+	
 	var aTags = document.querySelectorAll('.my-ads-layout > table > tbody > tr > td:nth-child(2) > .btn-toolbar > a:last-child');
 
 	// Method 1: Loop all
@@ -68,6 +70,7 @@ function loopPosts() {
 function loopAll(aTags) {
 
 	aTags.forEach(function(aTag) {
+		
 		var aHref = aTag.getAttribute('href');
 
 		wind.open(aHref);
@@ -92,10 +95,11 @@ function loopEachPosts(aTags, miliseconds) {
 		}, miliseconds);
 }
 
-
 // Reload page
 function reloadPage(miliseconds) {
+	
 	setTimeout(function() {
+		
 		location.reload();
 	}, miliseconds);
 }
